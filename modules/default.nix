eefaa279e0726486d39ce5a3c4f143103450d4cd
tatumon <tatumonar@proton.me>
2026-09-22 15:58:11 -0300

    Discovering new option function

--- modules/default.nix
+++ modules/default.nix
@@ -10,11 +10,7 @@ in
 {
   options.programs.caddy = {
     enable = lib.mkEnableOption "Caddy";
-    package = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.caddy;
-      description = "Caddy package to install.";
-    };
+    package = lib.mkPackageOption pkgs "caddy" { };
     configFile = lib.mkOption {
       type = lib.types.nullOr lib.types.lines;
       default = null;
